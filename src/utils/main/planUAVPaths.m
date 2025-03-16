2cb1e0b88fa976cab13bb54a30bf69e9b0dce091
You Ziang <1159375899@qq.com>
2025-03-16 19:41:47 +0800

    refactor(src): 优化 AUV 路径规划代码
    - 移除了未使用的注释代码块
    - 简化了部分变量的初始化
    - 优化了代码结构，提高了可读性
    - 调整了注释，使其更加清晰

--- src/utils/main/planUAVPaths.m
+++ src/utils/main/planUAVPaths.m
@@ -1,4 +1,4 @@
-    % 计划AUV路径
+% 计划AUV路径
     %
     % 功能描述：
     %   此函数用于生成AUV的路径规划。它读取CSV文件中的航点信息，
@@ -29,15 +29,8 @@
     %   作者：董星犴
     %   邮箱：1443123118@qq.com
     %   单位：哈尔滨工程大学
-function planUAVPaths(app,numLines,dubinsns,dubinsnl,dubinsnf)
-    % clear all;
-    % close all;
-    % clc;
-%     addpath('Function_Plot');
-%     addpath('Function_Dubins');
-%     addpath('Function_Trajectory');
-    % addpath('dubins_obstacle_avoidance');
 
+function planUAVPaths(app,numLines,dubinsns,dubinsnl,dubinsnf)
     % 从工作区获取Waypoints变量
     try
         Waypoints = evalin('base', 'Waypoints');
@@ -52,73 +45,66 @@ function planUAVPaths(app,numLines,dubinsns,dubinsnl,dubinsnf)
         errordlg('工作区中未找到circlesInfo变量', '错误');
         return;
     end
-    % % 加载圆的信息
-    % try
-    %     load('circlesInformation.mat');
-    % catch
-    %     errordlg('无法找到障碍物信息文件 circlesInformation.mat', '错误');
-    %     return;
-    % end
 
-    % Initialize Data
-    Property.obs_last=0;                                                % Record the obstacles avoided during current trajectory planning
-    Property.invasion=0;                                                % Record whether there is any intrusion into obstacles (threat areas) during trajectory planning
-    Property.mode=1;                                                    % Set trajectory generation mode 1: shortest path; 2: Conventional path
-    Property.ns=dubinsns;                                                     % Set the number of discrete points in the starting arc segment
-    Property.nl=dubinsnl;                                                     % Set the number of discrete points in the straight line segment
-    Property.nf=dubinsnf;                                                     % Set the number of discrete points at the end of the arc segment
-    Property.max_obs_num=5;                                             % Set the maximum number of obstacles to be detected for each path planning
-    Property.max_info_num=20;                                           % Set the maximum number of stored path segments for each planning step
-    Property.max_step_num=4;                                            % Set the maximum number of planned steps for the path
-    Property.Info_length=33;                                            % Set the length of each path information
-    Property.radius=100*1e3;                                            % Set the turning radius of the UAV（mm）
+    % 初始化数据
+    Property.obs_last=0;                                                % 记录当前轨迹规划期间避开的障碍物
+    Property.invasion=0;                                                % 记录轨迹规划期间是否有任何侵入障碍物（威胁区域）
+    Property.mode=1;                                                    % 设置轨迹生成模式 1: 最短路径; 2: 常规路径
+    Property.ns=dubinsns;                                               % 设置起始弧段的离散点数
+    Property.nl=dubinsnl;                                               % 设置直线段的离散点数
+    Property.nf=dubinsnf;                                               % 设置结束弧段的离散点数
+    Property.max_obs_num=5;                                             % 设置每次路径规划要检测的最大障碍物数量
+    Property.max_info_num=20;                                           % 设置每个规划步骤存储的最大路径段数
+    Property.max_step_num=4;                                            % 设置路径的最大规划步骤数
+    Property.Info_length=33;                                            % 设置每个路径信息的长度
+    Property.radius=100*1e3;                                            % 设置AUV的转弯半径（毫米）
     Property.scale=1;
-    Property.increment=20*1e3;                                          % Set the adjustment range of path lenth increment
-    Property.selection1=3;                                              % Set path filtering mode 1
-    Property.selection2=1;                                              % Set path filtering mode 2
-                                                                    % =1: The path does not intersect with obstacles
-                                                                    % =2: The turning angle of the path shall not exceed 3 * pi/2
-                                                                    % =3: Simultaneously satisfying 1 and 2
+    Property.increment=20*1e3;                                          % 设置路径长度增量的调整范围
+    Property.selection1=3;                                              % 设置路径过滤模式1
+    Property.selection2=1;                                              % 设置路径过滤模式2
+                                                                    % =1: 路径不与障碍物相交
+                                                                    % =2: 路径的转向角不超过3 * pi/2
+                                                                    % =3: 同时满足1和2
                                                                 
-    % Set starting point infomation
-    StartInfo=Waypoints(1:2*numLines-1,:);            % unit (mm)
+    % 设置起点信息
+    StartInfo=Waypoints(1:2*numLines-1,:);            % 单位（毫米）
 
-    % Set ending point information
-    FinishInfo=Waypoints(2:2*numLines,:);               % unit (mm)
+    % 设置终点信息
+    FinishInfo=Waypoints(2:2*numLines,:);               % 单位（毫米）
 
-    % Set obastacles (threat circle) information
+    % 设置障碍物（威胁圆）信息
     ObsInfo=circlesInfo;
     ObsInfo(:,3)=ObsInfo(:,3)+2;
-    [uav_num,~]=size(StartInfo);                                        % Obtain UAVs number
-    [obs_num,~]=size(ObsInfo);                                          % Obtain obstacles number
+    [uav_num,~]=size(StartInfo);                                        % 获取AUV数量
+    [obs_num,~]=size(ObsInfo);                                          % 获取障碍物数量
 
-    Coop_State(1:uav_num)=struct(...                                    % The structure of flight paths information for UAVs
-        'traj_length',[],...                                            % Array of all path length 
-        'traj_length_max',0,...                                         % Maximum path length
-        'traj_length_min',0,...                                         % Minimum path length
-        'TrajSeqCell',[],...                                            % Path sequence cell array
-        'ideal_length',0,...                                       % Expected path length
-        'optim_length',0,...                                            % Optimized path length
-        'traj_index_top',0,...                                          % Index of path that lenth is greater than and closest to the expected path length
-        'traj_index_bottom',0,...                                       % Index of path that lenth is shorter than and closest to the expected path length
-        'TrajSeq_Coop',[]);                                             % Matrix of cooperative path sequence
+    Coop_State(1:uav_num)=struct(...                                    % AUV运行路径信息的结构
+        'traj_length',[],...                                            % 所有路径长度的数组
+        'traj_length_max',0,...                                         % 最大路径长度
+        'traj_length_min',0,...                                         % 最小路径长度
+        'TrajSeqCell',[],...                                            % 路径序列单元数组
+        'ideal_length',0,...                                            % 期望路径长度
+        'optim_length',0,...                                            % 优化后的路径长度
+        'traj_index_top',0,...                                          % 长度大于且最接近期望路径长度的路径索引
+        'traj_index_bottom',0,...                                       % 长度小于且最接近期望路径长度的路径索引
+        'TrajSeq_Coop',[]);                                             % 协作路径序列矩阵
 
-%% Plan the path of each UAV from the starting point to the endpoint in sequence
-    for uav_index=1:2*numLines-1                                                   % Traverse each UAV
-        start_info=StartInfo(uav_index,:);                              % Obtain the starting point information of the UAV
-        finish_info=FinishInfo(uav_index,:);                            % Obtain the ending point information of the UAV
-        Property.radius=start_info(4);                                  % Set the turning radius of the UAV based on initial information
-        TrajSeqCell=Traj_Collection...                                  % Calculate all available flight paths for the UAV
+    %% 按顺序规划每个AUV从起点到终点的路径
+    for uav_index=1:2*numLines-1                                        % 遍历每个AUV
+        start_info=StartInfo(uav_index,:);                              % 获取AUV的起点信息
+        finish_info=FinishInfo(uav_index,:);                            % 获取AUV的终点信息
+        Property.radius=start_info(4);                                  % 根据初始信息设置AUV的转弯半径
+        TrajSeqCell=Traj_Collection...                                  % 计算AUV的所有可用运行路径
             (start_info,finish_info,ObsInfo,Property);                  
-        Coop_State(uav_index)=Coop_State_Update...                      % Select the basic path from the available flight paths
-            (TrajSeqCell,Coop_State(uav_index),ObsInfo,Property);       % and optimize the basic path to generate a cooperative path
+        Coop_State(uav_index)=Coop_State_Update...                      % 从可用运行路径中选择基本路径
+            (TrajSeqCell,Coop_State(uav_index),ObsInfo,Property);       % 并优化基本路径以生成协作路径
 
         Plot_Traj_Multi_Modification(TrajSeqCell,ObsInfo,Property);
         hold on;
     end
 
 
-    Plot_Traj_Coop(Coop_State,ObsInfo,Property,1,1);                    % Plot cooperative path planning results
+    Plot_Traj_Coop(Coop_State,ObsInfo,Property,1,1);                    % 绘制协作路径规划结果
     % app.drawPathsButton.Enable = 'on';
     app.SendLocalTCPButton.Enable = 'on';
     app.GenerateButton.Enable = 'on';
@@ -257,7 +243,6 @@ function planUAVPaths(app,numLines,dubinsns,dubinsnl,dubinsnf)
     grid(app.UIAxes2, 'on');
 
     % 添加图例
-%     legend(app.UIAxes2, '路径', '路径点', '起点', '终点', '障碍物');
     legend(app.UIAxes2, '路径',  '起点', '终点', '障碍物');
     % 保持图形
     hold(app.UIAxes2, 'off');
